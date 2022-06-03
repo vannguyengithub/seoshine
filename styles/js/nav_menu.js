68e81eab12288a81e30d51ad3d7deec9246c944d
vannguyengithub <vannguyengithub@gmail.com>
2022-06-03 21:16:39 +0700

    index

--- styles/js/nav_menu.js
+++ styles/js/nav_menu.js
@@ -1,17 +1,18 @@
-// var navMenu = $('#header__menu');
-// console.log(navMenu);
+var modalInfo = $('#modal__primary');
 
-// $(document).on('click', '.nav__menu',
-//     function(e) {
-//         navMenu.addClass('open');
-//         $('body').addClass('no-scroll');
-//     });
-// $(document).on('click', 'body',
-//     function(e) {
-//         navMenu.removeClass('open');
-//         $('body').removeClass('no-scroll');
-//     });
-// -----------
+$(document).on('click', '.btn--header',
+    function(e) {
+        modalInfo.addClass(' active');
+        $('body').addClass('no-scroll');
+    });
+$(document).on('click', '.modal__close',
+    function(e) {
+        modalInfo.removeClass('active');
+        $('body').removeClass('no-scroll');
+    });
+
+
+
 
 var elementAsideLeftBar = document.getElementById('header__menu');
 var triggerAside = document.getElementById('nav__menu');
